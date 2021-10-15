87838a5ffea26cb84ecd3d4e728b0c2833e9d4d0
Erik Egliens <erik@bellcom.ee>
2021-10-16 02:46:12 +0300

    wordEnd fun add

--- script.js
+++ script.js
@@ -12,11 +12,17 @@ const cSeconds = document.querySelector('span[data-cSeconds]');
 
 
 
-const wordEndMonths = document.querySelectorAll('span[data-wordEnd-month]');
-const wordEndDays = document.querySelectorAll('span[data-wordEnd-day]');
-const wordEndHours = document.querySelectorAll('span[data-wordEnd-hour]');
-const wordEndMinutes = document.querySelectorAll('span[data-wordEnd-minute]');
-const wordEndSeconds = document.querySelectorAll('span[data-wordEnd-second]');
+const wordEndMonthsW = document.querySelector('span[data-wordEnd-wmonth]');
+const wordEndDaysW = document.querySelector('span[data-wordEnd-wday]');
+const wordEndHoursW = document.querySelector('span[data-wordEnd-whour]');
+const wordEndMinutesW = document.querySelector('span[data-wordEnd-wminute]');
+const wordEndSecondsW = document.querySelector('span[data-wordEnd-wsecond]');
+
+const wordEndMonthsC = document.querySelector('span[data-wordEnd-cmonth]');
+const wordEndDaysC = document.querySelector('span[data-wordEnd-cday]');
+const wordEndHoursC = document.querySelector('span[data-wordEnd-chour]');
+const wordEndMinutesC = document.querySelector('span[data-wordEnd-cminute]');
+const wordEndSecondsC = document.querySelector('span[data-wordEnd-csecond]');
 
 
 const monthsArr = ["месяц", "месяца", "месяцев"]
@@ -44,12 +50,12 @@ let cDay = 6;
 
   function msCount(msEvent, $mo, $d, $h, $mi, $s) {
     let millisecondsTillComing = msEvent - currentTime;
-    let seconds = parseInt((millisecondsTillComing / 1000) % 60);
-    let minutes = parseInt((millisecondsTillComing / (1000 * 60)) % 60);
-    let hours = parseInt((millisecondsTillComing / (1000 * 60 * 60)) % 24);
-    let days = parseInt((millisecondsTillComing / (1000 * 60 * 60 * 24)) % 30);
     let months = parseInt((millisecondsTillComing / (1000 * 60 * 60 * 24 * 30)) % 12);
-      //add zero, if <10
+    let days = parseInt((millisecondsTillComing / (1000 * 60 * 60 * 24)) % 30);
+    let hours = parseInt((millisecondsTillComing / (1000 * 60 * 60)) % 24);
+    let minutes = parseInt((millisecondsTillComing / (1000 * 60)) % 60);
+    let seconds = parseInt((millisecondsTillComing / 1000) % 60);
+    //add zero, if <10
       hours = hours < 10 ? '0' + hours : hours;
       minutes = minutes < 10 ? '0' + minutes : minutes;
       seconds = seconds < 10 ? '0' + seconds : seconds;
@@ -59,6 +65,47 @@ let cDay = 6;
       $h.textContent = hours
       $mi.textContent = minutes
       $s.textContent = seconds
+
+      // function checkEnding (time, word, arrName) {
+      //   if(time === 1) {
+      //     word.textContent = arrName[0]
+      //   } else if (time >=2 && time < 5){
+      //     word.textContent = arrName[1]
+      //   } else {
+      //     word.textContent = arrName[2]
+      //   }
+      // }
+
+      // checkEnding(months ,wordEndMonthsW, monthsArr)
+      // checkEnding(months ,wordEndMonthsC, monthsArr)
+
+
+    //   function checkEnding() {
+    //     var num, result;
+    //     num = months;
+    
+    //     var string = num.toString();
+    //     var char = string.charAt(string.length-1);
+    
+    //     if (char == "1" && !(num == 11)) {
+    //         result = num + " пользователь в сети1.";
+    //     }  
+    //     else if (char == "2" && !(num == 12)) {
+    //         result = num + " пользователя в сети2.";
+    //     }
+    //     else if (char == "3" && !(num == 13)) {
+    //         result = num + " пользователя в сети3.";
+    //     }
+    //     else if (char == "4" && !(num == 14)) {
+    //         result = num + " пользователя в сети4.";
+    //     }  
+    //     else {
+    //         result = num + " пользователей в сети5.";
+    //     }
+    
+    //     wordEndMonthsW.innerHTML = result;     
+    // }  
+    // checkEnding() 
   }
   msCount(weddingDay, wMonths, wDays, wHours, wMinutes, wSeconds)
   msCount(comingDay, cMonths, cDays, cHours, cMinutes, cSeconds)
