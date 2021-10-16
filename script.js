2785163ab79cfbeecfeeee4b5fd8fb90907cd231
Erik Egliens <erik@bellcom.ee>
2021-10-16 04:01:46 +0300

    comments add

--- script.js
+++ script.js
@@ -25,13 +25,6 @@ const wordEndMinutesC = document.querySelector('span[data-wordEnd-cminute]');
 const wordEndSecondsC = document.querySelector('span[data-wordEnd-csecond]');
 
 
-const monthsArr = ["месяц", "месяца", "месяцев"]
-const dayArr = ["день", "дня", "дней"]
-const hourArr = ["час", "часа", "часов"]
-const minuteArr = ["минута", "минуты", "минут"]
-const secondArr = ["секунда", "секунды", "секунд"]
-
-
 function clock() {
   let currentTime = new Date();
 
@@ -55,14 +48,18 @@ let cDay = 6;
     let hours = parseInt((millisecondsTillComing / (1000 * 60 * 60)) % 24);
     let minutes = parseInt((millisecondsTillComing / (1000 * 60)) % 60);
     let seconds = parseInt((millisecondsTillComing / 1000) % 60);
+
+
     //add zero, if <10
       hours = hours < 10 ? '0' + hours : hours;
       minutes = minutes < 10 ? '0' + minutes : minutes;
       seconds = seconds < 10 ? '0' + seconds : seconds;
      
+
+    //declension of numerical values
       function declOfNum(n, text_forms) {  
         n = Math.abs(n) % 100; 
-        var n1 = n % 10;
+        let n1 = n % 10;
         if (n > 10 && n < 20) { return text_forms[2]; }
         if (n1 > 1 && n1 < 5) { return text_forms[1]; }
         if (n1 == 1) { return text_forms[0]; }
