5f0496066f431c2f371abc91fe75aba707994ece
Erik Egliens <erik@bellcom.ee>
2021-10-21 00:08:05 +0300

    check if mobile

--- script.js
+++ script.js
@@ -126,3 +126,22 @@ if (localStorage.getItem('visited')) {
 
 // let userLang = navigator.language || navigator.userLanguage;
 // alert('The language is: ' + userLang);
+
+
+  // const isMobile = ('ontouchstart' in document.documentElement && navigator.userAgent.match(/Mobi/));
+
+  // const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+  // if (isMobile) {
+  //   body.classList.add('mobile');
+  // }
+
+  function isMobile () {
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ||
+    (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.platform))) {
+      body.classList.add('mobile');
+    }
+    else {
+      body.classList.add('mobile');
+    };
+  };
+  isMobile()
