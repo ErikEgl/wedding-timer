13a7d442dac364c74a879a87da8bd3aab93733c4
Erik Egliens <erik@bellcom.ee>
2021-10-22 20:18:07 +0300

    textContent fix

--- src/assets/js/parts/textContent.js
+++ src/assets/js/parts/textContent.js
@@ -2,11 +2,16 @@
   //after party btn click toggle textcontent
   const partyBtn = document.querySelector('button[data-party]');
   const partyBtnTextContent = partyBtn.textContent;
+  const regularRegimeBtnTextContent = 'Нажми для обычного режима';
   function textContentToggle() {
-    if (partyBtn.textContent === partyBtnTextContent) {
-      partyBtn.innerHTML = 'Нажми для обычного режима';
+    if (partyBtn.textContent === partyBtnTextContent ) {
+      partyBtn.innerHTML = regularRegimeBtnTextContent;
     } else {
       partyBtn.innerHTML = partyBtnTextContent;
-    }
+    } 
   }
  
+
+  if (localStorage.getItem("visited-mobile") || localStorage.getItem("visited")) {
+    partyBtn.innerHTML = regularRegimeBtnTextContent;
+  }
