67ae047a5ac0ea12d82d1b4ba6c0ab797fb25612
Erik Egliens <erik@bellcom.ee>
2021-10-22 19:22:49 +0300

    delete commented code

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -181,19 +181,6 @@ function isMobileSetLocalStorageStyle() {
 }
 isMobileSetLocalStorageStyle();
 
-// let usrlang = navigator.languages;
-// console.log(usrlang);
-
-// let userLang = navigator.language || navigator.userLanguage;
-// alert('The language is: ' + userLang);
-
-// const isMobile = ('ontouchstart' in document.documentElement && navigator.userAgent.match(/Mobi/));
-
-// const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
-// if (isMobile) {
-//   body.classList.add('mobile');
-// }
-
 function isMobile() {
   if (
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
--- src/assets/js/parts/mobileCheck.js
+++ src/assets/js/parts/mobileCheck.js
@@ -1,16 +1,3 @@
-// let usrlang = navigator.languages;
-// console.log(usrlang);
-
-// let userLang = navigator.language || navigator.userLanguage;
-// alert('The language is: ' + userLang);
-
-// const isMobile = ('ontouchstart' in document.documentElement && navigator.userAgent.match(/Mobi/));
-
-// const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
-// if (isMobile) {
-//   body.classList.add('mobile');
-// }
-
 function isMobile() {
   if (
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
