857541c7b0adeda33d8d78a487e5cf77e278039f
Erik Egliens <erik@bellcom.ee>
2021-10-28 01:08:03 +0300

    howManyTimesWereOnSite tectContent change

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -329,7 +329,7 @@ function timesOnSite() {
     let loaded = parseInt( localStorage.getItem('loaded'), 10 ),
         loaded_numb = loaded ? loaded+1 : 1;
     localStorage.setItem('loaded', loaded_numb);
-    body.append('Счётчик посещений: ' + loaded_numb);
+    body.append('Счётчик посещений таймера: ' + loaded_numb);
 }
 timesOnSite()
 
--- src/assets/js/parts/howManyTimesWereOnSite.js
+++ src/assets/js/parts/howManyTimesWereOnSite.js
@@ -2,6 +2,6 @@ function timesOnSite() {
     let loaded = parseInt( localStorage.getItem('loaded'), 10 ),
         loaded_numb = loaded ? loaded+1 : 1;
     localStorage.setItem('loaded', loaded_numb);
-    body.append('Счётчик посещений: ' + loaded_numb);
+    body.append('Счётчик посещений таймера: ' + loaded_numb);
 }
 timesOnSite()
